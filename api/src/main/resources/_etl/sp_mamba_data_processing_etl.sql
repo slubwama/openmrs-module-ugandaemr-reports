ca0b5f1017f5f02e4b66566ce9e512199397b0c7
slubwama1 <lubwamasamuel3@gmail.com>
2025-11-18 19:01:13 +0300

    resolve conflicts

---
+++ api/src/main/resources/_etl/sp_mamba_data_processing_etl.sql
@@ -0,0 +1,21 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS sp_mamba_data_processing_etl;
+
+CREATE PROCEDURE sp_mamba_data_processing_etl(IN etl_incremental_mode INT)
+
+BEGIN
+    -- add base folder SP here if any --
+CALL sp_mamba_system_drop_fact_tables();
+CALL sp_data_processing_derived_transfers();
+CALL sp_data_processing_derived_non_suppressed();
+CALL sp_data_processing_derived_hiv_art_card();
+CALL sp_data_processing_derived_IIT();
+CALL sp_data_processing_derived_hts();
+
+END //
+
+DELIMITER ;
+
+
+    -- $END
