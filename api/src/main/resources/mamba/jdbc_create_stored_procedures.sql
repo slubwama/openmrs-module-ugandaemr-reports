809c6247fcd4a281fed5ba57cb76ec667499da0f
slubwama1 <lubwamasamuel3@gmail.com>
2026-01-21 10:04:54 +0300

    remove unnecessary changes

--- api/src/main/resources/mamba/jdbc_create_stored_procedures.sql
+++ api/src/main/resources/mamba/jdbc_create_stored_procedures.sql
@@ -14528,7 +14528,7 @@ CREATE PROCEDURE sp_mamba_data_processing_etl(IN etl_incremental_mode INT)
 
 BEGIN
     -- add base folder SP here if any --
-    CALL sp_mamba_system_drop_fact_tables();
+
     CALL sp_data_processing_derived_transfers();
     CALL sp_data_processing_derived_non_suppressed();
     CALL sp_data_processing_derived_hiv_art_card();
@@ -24758,6 +24758,271 @@ BEGIN
         completion_status          = 'ERROR',
         transaction_status         = 'COMPLETED',
         success_or_error_message   = CONCAT('sp_fact_test_orders_update', ', ', @mysql_errno, ', ', @message_text)
+        WHERE id = (SELECT last_etl_schedule_insert_id FROM _mamba_etl_user_settings ORDER BY id DESC LIMIT 1);
+
+    RESIGNAL;
+END;
+
+-- $BEGIN
+
+-- $END
+END;
+~-~-
+
+
+        
+-- ---------------------------------------------------------------------------------------------
+-- ----------------------  sp_fact_test_orders_results  ----------------------------
+-- ---------------------------------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS sp_fact_test_orders_results;
+
+
+~-~-
+CREATE PROCEDURE sp_fact_test_orders_results()
+BEGIN
+
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+    GET DIAGNOSTICS CONDITION 1
+
+    @message_text = MESSAGE_TEXT,
+    @mysql_errno = MYSQL_ERRNO,
+    @returned_sqlstate = RETURNED_SQLSTATE;
+
+    CALL sp_mamba_etl_error_log_insert('sp_fact_test_orders_results', @message_text, @mysql_errno, @returned_sqlstate);
+
+    UPDATE _mamba_etl_schedule
+    SET end_time                   = NOW(),
+        completion_status          = 'ERROR',
+        transaction_status         = 'COMPLETED',
+        success_or_error_message   = CONCAT('sp_fact_test_orders_results', ', ', @mysql_errno, ', ', @message_text)
+        WHERE id = (SELECT last_etl_schedule_insert_id FROM _mamba_etl_user_settings ORDER BY id DESC LIMIT 1);
+
+    RESIGNAL;
+END;
+
+-- $BEGIN
+CALL sp_fact_test_orders_results_create();
+CALL sp_fact_test_orders_results_insert();
+CALL sp_fact_test_orders_results_update();
+-- $END
+END;
+~-~-
+
+
+        
+-- ---------------------------------------------------------------------------------------------
+-- ----------------------  sp_fact_test_orders_results_create  ----------------------------
+-- ---------------------------------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS sp_fact_test_orders_results_create;
+
+
+~-~-
+CREATE PROCEDURE sp_fact_test_orders_results_create()
+BEGIN
+
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+    GET DIAGNOSTICS CONDITION 1
+
+    @message_text = MESSAGE_TEXT,
+    @mysql_errno = MYSQL_ERRNO,
+    @returned_sqlstate = RETURNED_SQLSTATE;
+
+    CALL sp_mamba_etl_error_log_insert('sp_fact_test_orders_results_create', @message_text, @mysql_errno, @returned_sqlstate);
+
+    UPDATE _mamba_etl_schedule
+    SET end_time                   = NOW(),
+        completion_status          = 'ERROR',
+        transaction_status         = 'COMPLETED',
+        success_or_error_message   = CONCAT('sp_fact_test_orders_results_create', ', ', @mysql_errno, ', ', @message_text)
+        WHERE id = (SELECT last_etl_schedule_insert_id FROM _mamba_etl_user_settings ORDER BY id DESC LIMIT 1);
+
+    RESIGNAL;
+END;
+
+-- $BEGIN
+CREATE TABLE mamba_fact_test_orders_results
+(
+    id        INT AUTO_INCREMENT,
+    test_orders_id INT NOT NULL,
+    encounter_id INT NULL,
+    encounter_datetime DATE NULL,
+    client_id INT NULL,
+    test_concept_id  INT NOT NULL,
+    test_parameter        VARCHAR(255) NULL,
+    test_value        TEXT NULL,
+
+        PRIMARY KEY (id)
+) CHARSET = UTF8;
+
+CREATE INDEX
+    mamba_fact_test_orders_client_id_index ON mamba_fact_test_orders_results (client_id);
+CREATE INDEX
+    mamba_fact_test_orders_order_id_index ON mamba_fact_test_orders_results (id);
+
+CREATE INDEX
+    mamba_fact_test_orders_test_order_results_id_index ON mamba_fact_test_orders_results (test_orders_id);
+
+
+-- $END
+END;
+~-~-
+
+
+        
+-- ---------------------------------------------------------------------------------------------
+-- ----------------------  sp_fact_test_orders_results_insert  ----------------------------
+-- ---------------------------------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS sp_fact_test_orders_results_insert;
+
+
+~-~-
+CREATE PROCEDURE sp_fact_test_orders_results_insert()
+BEGIN
+
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+    GET DIAGNOSTICS CONDITION 1
+
+    @message_text = MESSAGE_TEXT,
+    @mysql_errno = MYSQL_ERRNO,
+    @returned_sqlstate = RETURNED_SQLSTATE;
+
+    CALL sp_mamba_etl_error_log_insert('sp_fact_test_orders_results_insert', @message_text, @mysql_errno, @returned_sqlstate);
+
+    UPDATE _mamba_etl_schedule
+    SET end_time                   = NOW(),
+        completion_status          = 'ERROR',
+        transaction_status         = 'COMPLETED',
+        success_or_error_message   = CONCAT('sp_fact_test_orders_results_insert', ', ', @mysql_errno, ', ', @message_text)
+        WHERE id = (SELECT last_etl_schedule_insert_id FROM _mamba_etl_user_settings ORDER BY id DESC LIMIT 1);
+
+    RESIGNAL;
+END;
+
+-- $BEGIN
+INSERT INTO mamba_fact_test_orders_results(test_orders_id,
+                                          encounter_id,
+                                          encounter_datetime,
+                                          client_id,
+                                          test_concept_id,
+                                          test_parameter,
+                                          test_value)
+SELECT labtests.id                                                                         AS test_orders_id,
+       labtests.encounter_id,
+       encounter_datetime,
+       client_id,
+       labtests.concept_id                                                                 AS test_concept_id,
+       cn.name                                                                             AS test_parameter,
+       IF(cd.name = 'Numeric', value_numeric,
+          IF(cd.name = 'Date', DATE(value_datetime), IF(cd.name = 'Coded', cn1.name, ''))) AS value
+FROM (SELECT mfto.id,
+             mfto.encounter_id,
+             mfto.client_id,
+             o.obs_id,
+             DATE(obs_datetime) as encounter_datetime,
+             o.concept_id
+      FROM mamba_fact_test_orders mfto
+               LEFT JOIN obs o
+                         ON mfto.test_concept_id = o.concept_id AND mfto.encounter_id = o.encounter_id
+               INNER JOIN concept c ON o.concept_id = c.concept_id
+               INNER JOIN concept_class cc ON c.class_id = cc.concept_class_id
+      WHERE cc.name = 'LabSet'
+        AND c.is_set = 1
+        AND o.voided = 0) labtests
+         LEFT JOIN obs o ON o.obs_group_id = labtests.obs_id
+         LEFT JOIN concept_name cn ON o.concept_id = cn.concept_id
+         INNER JOIN concept c ON o.concept_id = c.concept_id
+         INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
+         left join concept_name cn1 on o.value_coded = cn1.concept_id AND
+                                       IF(cn1.locale_preferred = 1, cn1.locale_preferred = 1,
+                                          cn1.concept_name_type = 'FULLY_SPECIFIED')
+WHERE IF(cn.locale_preferred = 1, cn.locale_preferred = 1, cn.concept_name_type = 'FULLY_SPECIFIED');
+
+
+INSERT INTO mamba_fact_test_orders_results(test_orders_id,
+                                          encounter_id,
+                                          client_id,
+                                          test_concept_id,
+                                          test_parameter,
+                                          test_value)
+SELECT id                                                                                                    AS test_orders_id,
+       encounter_id,
+       client_id,
+       concept_id                                                                                            AS test_concept_id,
+       test_parameter,
+       IF(datatype = 'Numeric', value_numeric,
+          IF(datatype = 'Date', DATE(value_datetime), IF(datatype = 'Coded', coded_value_text, value_text))) AS value
+FROM (SELECT mfto.id,
+             mfto.encounter_id,
+             mfto.client_id,
+             o.concept_id,
+             cn.name  AS test_parameter,
+             cd.name  AS datatype,
+             cn1.name AS coded_value_text,
+             value_numeric,
+             value_datetime,
+             value_text
+
+      FROM mamba_fact_test_orders mfto
+               LEFT JOIN obs o
+                         ON mfto.test_concept_id = o.concept_id AND mfto.encounter_id = o.encounter_id
+               INNER JOIN concept c ON o.concept_id = c.concept_id
+               LEFT JOIN concept_name cn ON c.concept_id = cn.concept_id
+               INNER JOIN concept_datatype cd ON c.datatype_id = cd.concept_datatype_id
+               INNER JOIN concept_class cc ON c.class_id = cc.concept_class_id
+               LEFT JOIN concept_name cn1 ON o.value_coded = cn1.concept_id AND
+                                             IF(cn1.locale_preferred = 1, cn1.locale_preferred = 1,
+                                                cn1.concept_name_type = 'FULLY_SPECIFIED')
+      WHERE cc.name <> 'LabSet'
+        AND o.voided = 0
+        AND IF(cn.locale_preferred = 1, cn.locale_preferred = 1, cn.concept_name_type = 'FULLY_SPECIFIED')) labtests;
+
+
+-- $END
+END;
+~-~-
+
+
+        
+-- ---------------------------------------------------------------------------------------------
+-- ----------------------  sp_fact_test_orders_results_query  ----------------------------
+-- ---------------------------------------------------------------------------------------------
+
+
+
+
+        
+-- ---------------------------------------------------------------------------------------------
+-- ----------------------  sp_fact_test_orders_results_update  ----------------------------
+-- ---------------------------------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS sp_fact_test_orders_results_update;
+
+
+~-~-
+CREATE PROCEDURE sp_fact_test_orders_results_update()
+BEGIN
+
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+    GET DIAGNOSTICS CONDITION 1
+
+    @message_text = MESSAGE_TEXT,
+    @mysql_errno = MYSQL_ERRNO,
+    @returned_sqlstate = RETURNED_SQLSTATE;
+
+    CALL sp_mamba_etl_error_log_insert('sp_fact_test_orders_results_update', @message_text, @mysql_errno, @returned_sqlstate);
+
+    UPDATE _mamba_etl_schedule
+    SET end_time                   = NOW(),
+        completion_status          = 'ERROR',
+        transaction_status         = 'COMPLETED',
+        success_or_error_message   = CONCAT('sp_fact_test_orders_results_update', ', ', @mysql_errno, ', ', @message_text)
         WHERE id = (SELECT last_etl_schedule_insert_id FROM _mamba_etl_user_settings ORDER BY id DESC LIMIT 1);
 
     RESIGNAL;
